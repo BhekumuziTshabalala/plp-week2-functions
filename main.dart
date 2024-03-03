b6f6a38b0b795455731a42a74f1a731db34c3716
Bhekumuzi Tshabalala <Bhekumuzitshabalala.main@gmail.com>
2024-03-03 15:27:30 +0200

    main file

---
+++ main.dart
@@ -0,0 +1,24 @@
+int addTwo(int num1, int num2) {
+    return num1 + num2;
+  }
+
+int subractTwo(int num1, int num2) {
+    return num1 - num2;
+  }
+
+
+int multiplyTwo(int num1, int num2) {
+    return num1 * num2;
+  }
+
+double divideTwo(int num1, int num2) {
+    return num1 / num2;
+  }
+
+int stringLength(String word) {
+    return word.length;
+  }
+
+int getFirstElement(List element) {
+    return element[0];
+  }
